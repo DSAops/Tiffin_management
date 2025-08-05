693ce7ac1a445b3626ce638923ce1b649aa59662
Saksham <141105618+sckchcm-g@users.noreply.github.com>
2025-08-05 14:11:21 +0530

    Delete android directory

--- android/clear_data.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Clear user data file
-data_file = "user_data.json"
-if os.path.exists(data_file):
-    os.remove(data_file)
-    print("Cleared user data file")
-else:
-    print("No user data file found")
